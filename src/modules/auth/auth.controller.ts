ebc7bf4901abd0e57672d12ed9a215d0a79c2322
Mohammed-Hassan-Saad-Elmasry <mohammedhassanelmasry881@gmail.com>
2024-11-06 16:43:58 +0200

    ref

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,12 +1,12 @@
-import { Body, Controller, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDTO, signupDTO } from './validation/auth.DTO';
-import { ValidatiompipePipe } from 'src/pipe/validatiompipe/validatiompipe.pipe';
-import * as validators from '../auth/validation/auth.joi';
+// import * as validators from '../auth/validation/auth.joi';
+// import { ValidatiompipePipe } from 'src/pipe/validatiompipe/validatiompipe.pipe';
 @Controller('auth')
 export class AuthController {
   constructor(private _AuthService: AuthService) {}
-  @UsePipes(new ValidatiompipePipe(validators.singup))
+  // @UsePipes(new ValidatiompipePipe(validators.singup))
   @Post('signup')
   async signup(@Body() body: signupDTO) {
     return await this._AuthService.signup(body);
